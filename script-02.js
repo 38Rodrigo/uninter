954fce5783295608f20e92a7f1b569127a6813c1
rodrigoJesusdev <153654980+38Rodrigo@users.noreply.github.com>
2025-06-15 14:43:52 -0300

    adicionado novos funcoes do estudo

---
+++ script-02.js
@@ -0,0 +1,5 @@
+const campo1 = document.querySelector("campo1")
+const campo2 = document.querySelector("campo2")
+const seletor = document.querySelector("seletor")
+const botao = document.querySelector("botao")
+let resposta = document.querySelector("resposta")
